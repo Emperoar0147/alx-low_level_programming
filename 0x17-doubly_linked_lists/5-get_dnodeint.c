4073ad65a0ac0f3ee97d6217e44411e52f1dd568
“Sylvanus <sylvanusuzor@gmail.com>
2024-04-27 01:12:35 -0700

    update

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,15 +9,15 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    while (head != NULL)
-    {
-        if (i == index)
-            return (head);
-        i++;
-        head = head->next;
-    }
+	while (head != NULL)
+	{
+		if (i == index)
+		return (head);
+	i++;
+	head = head->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
