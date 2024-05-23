5375154857eac8fd30a9200db676fd44a1993b89
“Sylvanus <sylvanusuzor@gmail.com>
2024-05-22 21:42:16 -0700

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,50 +2,30 @@
 #include "search_algos.h"
 
 /**
- * print_array - Prints an array of integers
- * @array: The array to be printed
- * @left: The starting index
- * @right: The ending index
- */
-void print_array(int *array, size_t left, size_t right)
-{
-    size_t i;
-
-    printf("Searching in array: ");
-    for (i = left; i <= right; i++)
-    {
-        printf("%d", array[i]);
-        if (i < right)
-            printf(", ");
-    }
-    printf("\n");
-}
-
-/**
- * binary_search - Searches for a value in a sorted array of integers using the Binary search algorithm
- * @array: A pointer to the first element of the array to search in
- * @size: The number of elements in the array
- * @value: The value to search for
+ * binary_search - Searches for a value in a sorted array of integers
+ *                 using the Binary search algorithm.
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in array.
+ * @value: The value to search for.
  *
- * Return: The index where the value is located, or -1 if the value is not present or the array is NULL
+ * Return: If the value is not present or the array is NULL, -1.
+ *         Otherwise, the index where the value is located.
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left, right, mid;
+    size_t left = 0, right = size - 1, mid;
 
     if (array == NULL)
         return (-1);
 
-    left = 0;
-    right = size - 1;
-
     while (left <= right)
     {
         print_array(array, left, right);
+
         mid = (left + right) / 2;
 
         if (array[mid] == value)
-            return ((int)mid);
+            return (mid);
         else if (array[mid] < value)
             left = mid + 1;
         else
@@ -53,4 +33,22 @@ int binary_search(int *array, size_t size, int value)
     }
 
     return (-1);
+}
+
+/**
+ * print_array - Prints the elements of an array within the given range.
+ * @array: A pointer to the first element of the array.
+ * @start: The index to start printing from.
+ * @end: The index to stop printing at.
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+    size_t i;
+
+    printf("Searching in array: ");
+    for (i = start; i < end; i++)
+    {
+        printf("%d, ", array[i]);
+    }
+    printf("%d\n", array[i]);
 }
